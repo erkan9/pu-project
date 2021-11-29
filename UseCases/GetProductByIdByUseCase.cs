0f8506437081f4c198b06592ffe3ef709f738f6f
Erkan Kamber <erkan_kamber@abv.bg>
2021-11-29 14:41:58 +0200

    FInished adding and editing Products

---
+++ UseCases/GetProductByIdByUseCase.cs
@@ -0,0 +1,25 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
+
+namespace UseCases
+{
+    public class GetProductByIdByUseCase : IGetProductByIdByUseCase
+    {
+        private readonly IProductRepository productRepository;
+
+        public GetProductByIdByUseCase(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public Product Execute(int productId)
+        {
+            return productRepository.GetProductById(productId);
+        }
+    }
+}
---
+++ UseCases/UseCaseInterfaces/IGetProductByIdByUseCase.cs
@@ -0,0 +1,9 @@
+﻿using CoreBusiness;
+
+namespace UseCases
+{
+    public interface IGetProductByIdByUseCase
+    {
+        Product Execute(int productId);
+    }
+}
